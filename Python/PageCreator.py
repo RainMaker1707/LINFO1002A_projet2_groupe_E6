29f6521f828cb8937c95c2fd6bac39124019a650
Hadrien Allegaert <hadrien.allegaert@student.uclouvain.be>
2020-03-17 22:12:34 +0100

    Data files add

--- Python/PageCreator.py
+++ Python/PageCreator.py
@@ -1,11 +1,12 @@
+import Python.PageReader as Page
 from flask import Flask
-
 app = Flask(__name__)
 
 
 @app.route('/')
-def test():
-    return '<html><title>AAAA</title></html>'
+def home():
+    content_str = Page.read_file("./Html/home.html")
+    return content_str
 
 
-print(test())
+print(home())
