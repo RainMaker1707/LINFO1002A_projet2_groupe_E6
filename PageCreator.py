d4d19ea5ace9c7a80653587fd4e5c6c40fa20f2d
Hadrien Allegaert <hadrien.allegaert@student.uclouvain.be>
2020-03-18 21:25:56 +0100

    Refactor files to easier import from flask

--- PageCreator.py
+++ PageCreator.py
@@ -1,8 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
-
-
 def read_file(path):
+    """
+    :param path: string path to the file html
+    :return:  the string content of the file
+    """
     content_str = str()
     with open(path, 'r') as file:
         for line in file:
@@ -36,21 +36,3 @@ def make_title(html_content):
     :return: an HTML string to config the title of the slide in the nav board
     """
     return html_content.replace("TITLE", "<title>Coronavirus CoViD-19 propagation</title>")
-
-
-@app.route('/')
-def home():
-    content_str = read_file("Html/home.html")
-    content_str = make_style(content_str, "CSS/home.css")
-    content_str = make_menu(content_str)
-    content_str = make_title(content_str)
-    return content_str.replace('X', "<body><u>CHECK HOMEPAGE</u></body>")
-
-
-@app.route("/graph.html")
-def graph():
-    content_str = read_file("Html/graph.html")
-    content_str = make_style(content_str)
-    content_str = make_title(content_str)
-    content_str = make_menu(content_str)
-    return content_str.replace('X', "<body><u>CHECK GRAPH PAGE</u></body>")
