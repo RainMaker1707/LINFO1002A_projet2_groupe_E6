d288359324e669121713318798c8536effb7dee6
Hadrien Allegaert <hadrien.allegaert@student.uclouvain.be>
2020-03-19 20:53:52 +0100

    CSS tests + PEP8 Mux correction

--- TP1/Mux/test_read.py
+++ TP1/Mux/test_read.py
@@ -7,9 +7,8 @@ def get_data(path_file):
         with open(path_file, 'r') as file:
             return file.readlines()
     except FileNotFoundError:
-        with open(path_file, 'w') as file:
-            file.close()
-        return get_data(path_file)
+        return False
+
 
 def get_script(path):
     """
@@ -17,12 +16,13 @@ def get_script(path):
     """
     content_str = ""
     try:
-        with open(path, 'r') as file:
-            for line in file:
+        with open(path, 'r') as file_in:
+            for line in file_in:
                 content_str += line
-    except:
-        pass
+    except FileNotFoundError:
+        return -1
     return content_str
+
 
 def format_data(heavy_str_data):
     """
@@ -34,44 +34,48 @@ def format_data(heavy_str_data):
         temp_str = ""
         for j in range(len(heavy_str_data[i])):
             if heavy_str_data[i][j] == ',':
-            	if temp_str == "":
-            		ret_lst[i].append(0)
-            	else:
-            		try:
-            			ret_lst[i].append(int(temp_str))
-	            		temp_str = ""
-	            	except:
-	            		ret_lst[i].append(temp_str)
-	            		temp_str = ""
+                if temp_str == "":
+                    ret_lst[i].append(0)
+                else:
+                    try:
+                        ret_lst[i].append(int(temp_str))
+                        temp_str = ""
+                    except ValueError:
+                        ret_lst[i].append(temp_str)
+                        temp_str = ""
             else:
                 temp_str += heavy_str_data[i][j]
     return ret_lst
 
+
 def reverse(matrix):
-	ret = [[] for _ in range(len(matrix[0]))]
-	for i in range(len(matrix[0])):
-		temp = []
-		for j in range(len(matrix)):
-			temp.append(matrix[j][i])
-			ret[i] = temp
-	return ret
+    ret = [[] for _ in range(len(matrix[0]))]
+    for i in range(len(matrix[0])):
+        temp = []
+        for j in range(len(matrix)):
+            temp.append(matrix[j][i])
+            ret[i] = temp
+    return ret
 
-script = get_script("maxime.html")
-data = reverse(format_data(get_data("total_cases.csv")))
 
-#"{label: {0}, fill: false, borderColor: 'red', data: {1}, spanGaps: true}"
+def main():
+    script = get_script("maxime.html")
+    data = reverse(format_data(get_data("total_cases.csv")))
+    #  Js string : "{label: {0}, fill: false, borderColor: 'red', data: {1}, spanGaps: true}"
+    dates = data[0][1:]
+    dataset = ""
+    for j, i in enumerate(data[1:]):
+        dataset += "{"+'label: "{0}",\n fill: false,\n borderColor: "red",\n data: {1},\n spanGaps: true,\n'.\
+            format(i[0], i[1:])+"}"
+        if j < len(data)-1:
+            dataset += ", "
 
-dates = data[0][1:]
+    script = script.replace("#data#", dataset)
+    script = script.replace("#labels#", str(dates))
 
-datasets = ""
+    with open("out.html", 'w') as file:
+        file.write(script)
+    return
 
-for j,i in enumerate(data[1:]):
-	datasets += "{"+'label: "{0}",\n fill: false,\n borderColor: "red",\n data: {1},\n spanGaps: true,\n'.format(i[0],i[1:])+"}"
-	if j < len(data)-1:
-		datasets += ", "
 
-script = script.replace("#data#",datasets)
-script = script.replace("#labels#",str(dates))
-
-with open("out.html", 'w') as file:
-	file.write(script)
+main()
