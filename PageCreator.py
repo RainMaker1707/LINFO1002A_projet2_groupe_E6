267961486a30c637131d88b3b3b2b6b467f2cedd
Hadrien Allegaert <hadrien.allegaert@student.uclouvain.be>
2020-03-19 14:33:01 +0100

    CSS files linked + menu update
    I added css files and linked them in their html file parent, for the moment i choose to use one css file per one html file we'll can try to use one css more file which contains the redundant style functions

--- PageCreator.py
+++ PageCreator.py
@@ -10,24 +10,19 @@ def read_file(path):
     return content_str
 
 
-def make_style(html_content, css_file=None):
-    """
-    :param html_content: string content of the html file
-    :param css_file:
-    :return: and HTML string to config the style sheet
-    """
-    if css_file is None:
-        return html_content.replace("STYLE_LINK", " ")
-    else:
-        return html_content.replace("STYLE_LINK", "<link href={0} ref=stylesheet>".format(css_file))
-
-
 def make_menu(html_content):
     """
     :param html_content: string content of the html file
     :return:
     """
-    return html_content.replace("MENU", "<ul><li><a href=\"graph.html\">Graph</a></li></ul>")
+    menu = ""
+    menu += "<ul id=\"menu\">"  # menu start
+    menu += "<li><a href=\"home.html\">Home</a></li>"
+    menu += "<li><a href=\"graph.html\">Graph</a></li>"
+    menu += "<li><a href=\"coursesList.html\">Courses</a></li>"
+    menu += "</ul>"  # menu end
+    print(menu)
+    return html_content.replace("MENU", menu)
 
 
 def make_title(html_content):
@@ -35,4 +30,4 @@ def make_title(html_content):
     :param html_content:
     :return: an HTML string to config the title of the slide in the nav board
     """
-    return html_content.replace("TITLE", "<title>Coronavirus CoViD-19 propagation</title>")
+    return html_content.replace("TITLE", "<title> Inginious submission stats</title>")
--- Route.py
+++ Route.py
@@ -5,16 +5,21 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home.html')
 def home():
     """
     Home page from the site
     :return:
     """
     content_str = read_file("Html/home.html")
-    content_str = make_style(content_str, "CSS/home.css")
     content_str = make_menu(content_str)
     content_str = make_title(content_str)
     return content_str.replace('X', "<body><u>CHECK HOMEPAGE</u></body>")
+
+
+@app.route('/CSS/home.css')
+def home_style():
+    return read_file("CSS/home.css")
 
 
 @app.route("/graph.html")
@@ -23,7 +28,26 @@ def graph():
     :return: Graph page test for an interactive graph
     """
     content_str = read_file("Html/graph.html")
-    content_str = make_style(content_str)
     content_str = make_title(content_str)
     content_str = make_menu(content_str)
     return content_str.replace('X', "<body><u>CHECK GRAPH PAGE</u></body>")
+
+
+@app.route('/CSS/graph.css')
+def graph_style():
+    return read_file("CSS/graph.css")
+
+
+@app.route('/coursesList.html')
+def courses():
+    """
+    :return:
+    """
+    content = make_menu(read_file("Html/coursesList.html"))
+
+    return make_title(content)
+
+
+@app.route('/CSS/coursesList.css')
+def courses_style():
+    return read_file("CSS/coursesList.css")
