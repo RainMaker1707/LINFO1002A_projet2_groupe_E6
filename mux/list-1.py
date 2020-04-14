7a9219c73d1026e48ee1349b333ccbba90813f91
mux99 <mux99@live.be>
2020-04-14 21:47:52 +0200

    tops frist draft
    actually empty

---
+++ mux/list-1.py
@@ -0,0 +1,14 @@
+import sqlite3
+from fcts import get_entries
+
+    return lst
+
+def top_winrate(filename: str, reversed=False, *course: str, **task:str):
+	"""
+	"""
+	
+
+def top_subs_count(filename: str, reversed=False, *course: str, **task:str):
+	"""
+	"""
+	pass
